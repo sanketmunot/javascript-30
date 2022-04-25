ee12417738fc52dc0d94fc2fb32faf6cb562608e
sanketmunot <sanketmunot95@gmail.com>
2022-04-25 22:33:34 +0530

    feat: completed drum kit app
    completed a working drumkit app

---
+++ 01-Drum-Kit/script.js
@@ -0,0 +1,20 @@
+function getKeyInput(e) {
+  let element = document.querySelector(`[data-key="${e.keyCode}"]`);
+
+  if (!element) return;
+  
+  let audioElement = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+  
+  element.classList.add('playing')
+  audioElement.currentTime = 0
+  audioElement.play()
+}
+
+function setKeyBackToNormal(e){
+  this.classList.remove('playing')
+}
+
+const keys = document.querySelectorAll('.item')
+
+keys.forEach(key => key.addEventListener("transitionend",setKeyBackToNormal))
+window.addEventListener("keydown", getKeyInput);
