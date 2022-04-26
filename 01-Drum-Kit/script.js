e18415ae50f3ff5c54fafc49b2daa463d910bfad
sanketmunot <sanketmunot95@gmail.com>
2022-04-26 10:51:41 +0530

    fix: code lint

--- 01-Drum-Kit/script.js
+++ 01-Drum-Kit/script.js
@@ -2,19 +2,21 @@ function getKeyInput(e) {
   let element = document.querySelector(`[data-key="${e.keyCode}"]`);
 
   if (!element) return;
-  
-  let audioElement = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-  
-  element.classList.add('playing')
-  audioElement.currentTime = 0
-  audioElement.play()
+
+  let audioElement = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+
+  element.classList.add("playing");
+  audioElement.currentTime = 0;
+  audioElement.play();
 }
 
-function setKeyBackToNormal(e){
-  this.classList.remove('playing')
+function setKeyBackToNormal(e) {
+  this.classList.remove("playing");
 }
 
-const keys = document.querySelectorAll('.item')
+const keys = document.querySelectorAll(".item");
 
-keys.forEach(key => key.addEventListener("transitionend",setKeyBackToNormal))
+keys.forEach((key) =>
+  key.addEventListener("transitionend", setKeyBackToNormal)
+);
 window.addEventListener("keydown", getKeyInput);
